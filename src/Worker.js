e0b6099de154468e135e4833f4e62bac8dded976
Manuel Baclet <manuel.baclet@dashdoc.eu>
2021-11-01 20:23:36 +0100

    Improve readability

--- src/Worker.js
+++ src/Worker.js
@@ -9,23 +9,12 @@ class Worker {
     async start() {
         console.log(this.name, "pulls data from", this.input.name);
         const data = await this.input.pull();
-        console.log(this.name, "has pulled", data.id, "from", this.input.name);
+        const { id } = data;
+        console.log(this.name, "has pulled", id, "from", this.input.name);
         const processedData = await this.process(data);
-        console.log(
-            this.name,
-            "pushes",
-            processedData.id,
-            "to",
-            this.output.name
-        );
+        console.log(this.name, "pushes", id, "to", this.output.name);
         await this.output.push(processedData);
-        console.log(
-            this.name,
-            "has pushed",
-            processedData.id,
-            "to",
-            this.output.name
-        );
+        console.log(this.name, "has pushed", id, "to", this.output.name);
 
         return this.start();
     }
